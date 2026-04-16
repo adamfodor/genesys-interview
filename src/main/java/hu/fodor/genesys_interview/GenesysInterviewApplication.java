d5efdde617dd4439ecc9161519f484c8cfbcb5e1
Adam Fodor <fodoradi007@gmail.com>
2026-04-16 12:47:54 +0200

    project creation

---
+++ src/main/java/hu/fodor/genesys_interview/GenesysInterviewApplication.java
@@ -0,0 +1,13 @@
+package hu.fodor.genesys_interview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesysInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenesysInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/fodor/genesys_interview/GenesysInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.fodor.genesys_interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenesysInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
